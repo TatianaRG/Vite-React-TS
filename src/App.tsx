48e5242a839172157ba412f5de2a77865d52ee98
tatiana <tatiana.rosca.guzun@gmail.com>
2023-04-23 21:28:39 +0100

    completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
 
 function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
 
-  return (
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
     <>
-    <div className="App">Hello</div>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
     </>
   )
+
+  return content
 }
 
-export default App
+export default App;
